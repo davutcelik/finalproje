bb9ba9042782388f2040df78bdffde806ecc3ea2
davutcelik <davutcelik5534@gmail.com>
2017-01-10 00:07:12 +0300

    kullanıcı girisi hazırlandı

--- finalproje/finalproje/Pages/Home.xaml.cs
+++ finalproje/finalproje/Pages/Home.xaml.cs
@@ -29,7 +29,36 @@ namespace finalproje.Pages
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            if (txtkad.Text == "DAVUT" && txtsifre.Password == "004")
+            {
+                FirstFloor.ModernUI.Presentation.LinkGroup menuler = new FirstFloor.ModernUI.Presentation.LinkGroup();
 
+                FirstFloor.ModernUI.Presentation.Link menuekle = new FirstFloor.ModernUI.Presentation.Link();
+                FirstFloor.ModernUI.Presentation.Link ekle = new FirstFloor.ModernUI.Presentation.Link();
+                FirstFloor.ModernUI.Presentation.Link sil = new FirstFloor.ModernUI.Presentation.Link();
+
+                menuekle.DisplayName = "KİTAP KİRALAMA SAYFASI";
+                ekle.DisplayName = " KİTAP EKLEME SAYFASI";
+                sil.DisplayName = " KİTAP SİLME SAYFASI";
+
+
+                menuekle.Source = new Uri("Pages/girissayfasi.xaml", UriKind.Relative);
+                ekle.Source = new Uri("Pages/kitapekle.xaml", UriKind.Relative);
+                sil.Source = new Uri("Pages/kitapsil.xaml", UriKind.Relative);
+
+                menuler.Links.Add(menuekle);
+                menuler.Links.Add(ekle);
+                menuler.Links.Add(sil);
+
+                MainWindow ab = Application.Current.MainWindow as MainWindow;
+                ab.MenuLinkGroups.Clear();
+                ab.MenuLinkGroups.Add(menuler);
+            }
+            else
+            {
+                MessageBox.Show("Hatalı Giriş Denemesi");
+            }
         }
     }
-}
+    }
+
